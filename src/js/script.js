b320303057f46fd904035d2bc085e923f8a4b4d5
Andrew Sakhnovskyi <samuelson1187@gmail.com>
2019-01-31 14:17:25 +0200

    second changes

---
+++ src/js/script.js
@@ -0,0 +1,74 @@
+'use strict';
+
+$(document).ready(function(){
+
+    var app = new Vue({
+        el: '#app',
+        data: {
+            name: '',
+            currencyfrom : [],
+            currencyto : [],
+            convertfrom: 'USD',
+            convertto: 'BTC',
+            amount : ''
+        },
+        computed : {
+            finalamount:function() {
+                var to = this.convertto;
+                var from = this.convertfrom;
+                var final;
+                switch(from) {
+                    case 'USD':
+                    if (to == 'BTC') {
+                        final = this.amount / 7000;
+                    }
+                    if (to == 'LTC') {
+                        final = this.amount / 200;
+                    }
+                    if (to == 'COI') {
+                        final = this.amount / 50;
+                    }
+                    break;
+                    case "GBR":
+                    if (to == "BTC") {
+                        final = this.amount / 6900;
+                    }
+                    if (to == "LTC") {
+                        final = this.amount / 190;
+                    }
+                    if (to == 'COI') {
+                        final = this.amount / 45;
+                    }
+                    break;
+                    case 'UAH':
+                    if (to == 'BTC') {
+                        final = this.amount / 7000;
+                    }
+                    if (to == 'LTC') {
+                        final = this.amount / 200;
+                    }
+                    if (to == 'COI') {
+                        final = this.amount / 50;
+                    }
+                    break;
+                }
+                return final;
+            }
+        },
+        mounted: function () {
+            var self = this;
+            $.ajax({
+                url: 'https://api.myjson.com/bins/leeas',
+                method: 'GET',
+                success: function (data) {
+                    self.currencyfrom = data.currencyfrom;
+                    self.currencyto = data.currencyto;
+                },
+                error: function (error) {
+                    console.log(error);
+                }
+            });
+        }
+    });
+});
+
